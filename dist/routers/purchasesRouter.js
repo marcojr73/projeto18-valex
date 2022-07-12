761b295d67355ec759607afc58e3e782d7bc6957
Marco Júnior Lopes de Melo <93060180+marcojr73@users.noreply.github.com>
2022-07-12 14:03:50 -0300

    Delete dist directory

--- dist/routers/purchasesRouter.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { purchase } from "../controllers/purchasesController.js";
-var purchaseRouter = Router();
-purchaseRouter.post("/purchase", purchase);
-export default purchaseRouter;
--- dist/routers/reloadRouter.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { reloadCard } from "../controllers/reloadController.js";
-var reloadRouter = Router();
-reloadRouter.post("/reload", reloadCard);
-export default reloadRouter;
